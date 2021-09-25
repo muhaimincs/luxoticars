e7c3ca1408744501ae1691cacbce7f939b3c5e01
Muhaimin CS <muhaimincs@gmail.com>
2021-09-25 12:21:19 +0800

    fix layout gallery

--- components/youtube-homepage.js
+++ components/youtube-homepage.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import { useDimensions } from 'react-hook-dimensions'
+import Link from 'next/link'
 
 function Yt ({ data }) {
   const [elementRef, elementDimensions, updateElementDimensions] = useDimensions({
@@ -74,16 +75,17 @@ export default function YoutubePage () {
         </div>
         <div className="py-8 max-w-sm mx-auto">
           <div className="flex items-center rounded-md shadow">
-            <a
-              href="https://youtube.com/luxoticars"
-              className="inline-flex w-full items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700"
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-            Subscribe to our channel
-            </a>
+            <Link href="/gallery">
+              <a
+                className="inline-flex w-full items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                Visit our gallery
+              </a>
+            </Link>
           </div>
         </div>
       </div>
--- pages/gallery.js
+++ pages/gallery.js
@@ -63,6 +63,20 @@ export default function GalleryPage ({ post, blockMap }) {
             />
           )}
         </article>
+        <div className="pb-52 max-w-sm mx-auto clear-both">
+          <div className="flex items-center rounded-md shadow">
+            <a
+              href="https://www.youtube.com/luxoticars"
+              className="inline-flex w-full items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+              Subscribe to our Youtube Channel
+            </a>
+          </div>
+        </div>
       </div>
     </>
   )
