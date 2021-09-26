dfbead13b45a0d6bce03bfab5b7072883a87125a
Muhaimin CS <muhaimincs@gmail.com>
2021-09-26 14:20:19 +0800

    fix link

--- components/carousel.homepage.js
+++ components/carousel.homepage.js
@@ -32,15 +32,17 @@ const Carousel = () => {
           disableOnInteraction: false
         }}
       >
-        <SwiperSlide data-swiper-autoplay="5500">
-          <Image
-            width={1280}
-            height={720}
-            layout="fill"
-            objectFit="cover"
-            // placeholder="blur"
-            src="https://images.ctfassets.net/ijuxqf6x1pz2/1N8YjfxoUBbnIdKnQtsZ0k/98bf4396c6cc3c4a323a33fae9accb49/photo_2021-09-24_09.31.08.jpeg" alt="Luxoticars Ferrari"
-          />
+        <SwiperSlide data-swiper-autoplay="10500">
+          <div className="relative">
+            <Image
+              width={1280}
+              height={720}
+              layout="responsive"
+              objectFit="cover"
+              // placeholder="blur"
+              src="https://images.ctfassets.net/ijuxqf6x1pz2/1N8YjfxoUBbnIdKnQtsZ0k/98bf4396c6cc3c4a323a33fae9accb49/photo_2021-09-24_09.31.08.jpeg" alt="Luxoticars Ferrari"
+            />
+          </div>
         </SwiperSlide>
         <SwiperSlide data-swiper-autoplay="30000">
           <video
@@ -61,14 +63,16 @@ const Carousel = () => {
           </video>
         </SwiperSlide>
         <SwiperSlide data-swiper-autoplay="5500">
-          <Image
-            width={1280}
-            height={720}
-            layout="fill"
-            objectFit="cover"
-            // placeholder="blur"
-            src="https://images.ctfassets.net/ijuxqf6x1pz2/15nQQBgBkiECSSQznnm5lV/1178ec83805376fbad23be545a9c7503/photo_2021-09-24_09.31.11.jpeg" alt="Luxoticars Ferrari"
-          />
+          <div className="relativew-full h-1/2">
+            <Image
+              width={1280}
+              height={720}
+              layout="responsive"
+              objectFit="cover"
+              // placeholder="blur"
+              src="https://images.ctfassets.net/ijuxqf6x1pz2/15nQQBgBkiECSSQznnm5lV/1178ec83805376fbad23be545a9c7503/photo_2021-09-24_09.31.11.jpeg" alt="Luxoticars Ferrari"
+            />
+          </div>
         </SwiperSlide>
       </Swiper>
     </div>
