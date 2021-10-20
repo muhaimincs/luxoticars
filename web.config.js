30f524c6e4252dff493a546342b7f65d83feaaf4
Muhaimin CS <muhaimincs@gmail.com>
2021-10-20 11:16:27 +0800

    add address

--- web.config.js
+++ web.config.js
@@ -2,6 +2,15 @@ const WEB = {
   name: 'LUXOTICARS',
   lang: 'en-US',
   link: 'https://luxoticars.cc',
+  description: 'The Syndicate Carlifestyle Cartel',
+  telephone: '+6017 328 3839',
+  address: {
+    streetAddress: 'No 253 Jalan Ampang Hilir Off Jalan U-Thant 50450 Kuala Lumpur',
+    addressLocality: 'Kuala Lumpur',
+    addressRegion: 'KL',
+    postalCode: '50450',
+    addressCountry: 'MY'
+  },
   postsPerPage: 6,
   sortByDate: false,
   notionPageId: process.env.NOTION_PAGE_ID,
