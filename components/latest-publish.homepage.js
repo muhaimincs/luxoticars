7ff6f917e6120798a33ad80a9a4513e1709e19dd
Muhaimin CS <muhaimincs@gmail.com>
2021-10-20 12:47:50 +0800

    add latest publish

---
+++ components/latest-publish.homepage.js
@@ -0,0 +1,109 @@
+import { useMemo } from 'react'
+import Image from 'next/image'
+
+import formatDate from '../lib/formatDate'
+import WEB from '../web.config'
+
+export default function LatestPublish ({ post }) {
+  const brand = require(`../public/brands/colors/${post?.tags[0]}.svg`)
+  const name = `${post?.Year} ${post?.title}`
+  const photos = post?.['Photo Gallery'].split(',')
+  const mileage = post?.Mileage
+  const exteriorColor = post?.exterior_color
+  const renderSubPhotosClassnames = useMemo(() => {
+    if (photos[3]) {
+      return 'grid grid-cols-3 gap-0'
+    }
+    return 'grid grid-cols-2 gap-0'
+  }, [photos])
+  return (
+    <div className="relative sm:py-16 lg:py-0">
+      <div aria-hidden="true" className="hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen">
+        <div className="absolute inset-y-0 right-1/2 w-full bg-gray-900/70 rounded-r-3xl lg:right-72" />
+        <svg
+          className="absolute top-8 left-1/2 -ml-3 lg:-right-8 lg:left-auto lg:top-12"
+          width={404}
+          height={392}
+          fill="none"
+          viewBox="0 0 404 392"
+        >
+          <defs>
+            <pattern
+              id="02f20b47-fd69-4224-a62a-4c9de5c763f7"
+              x={0}
+              y={0}
+              width={20}
+              height={20}
+              patternUnits="userSpaceOnUse"
+            >
+              <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect width={404} height={392} fill="url(#02f20b47-fd69-4224-a62a-4c9de5c763f7)" />
+        </svg>
+      </div>
+      <div className="relative mx-auto max-w-md px-3 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
+        <div className="relative rounded-t-xl overflow-hidden">
+          <div className="relative h-64 w-auto">
+            <Image
+              src={photos[0]}
+              alt={`Latest car on ${WEB.name}`}
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div className="absolute bottom-[20px] left-0 m-3 h-12">
+            <Image
+              src={brand}
+              alt="Workcation"
+            />
+          </div>
+        </div>
+        <div className={renderSubPhotosClassnames}>
+          <div className="relative h-32 w-auto">
+            <Image
+              src={photos[1]}
+              alt={`Latest car on ${WEB.name}`}
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div className="relative h-32 w-auto">
+            <Image
+              src={photos[2]}
+              alt={`Latest car on ${WEB.name}`}
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          {photos[3] && (
+            <div className="relative h-32 w-auto">
+              <Image
+                src={photos[0]}
+                alt={`Latest car on ${WEB.name}`}
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+          )}
+        </div>
+        <div className="relative py-8 px-3 bg-[rgba(25,26,26,1)] rounded-b-xl">
+          <blockquote>
+            <p className="relative text-xl font-medium text-white md:flex-grow border-b box-content pb-5">{name}</p>
+          </blockquote>
+          <div className="my-3 grid grid-cols-3 gap-2">
+            <p className="text-white tabular-nums box-content border-b pb-3">
+              {formatDate(post?.date?.start_date || post?.createdTime, 'en')}
+            </p>
+            <p className="text-white tabular-nums ordinal slashed-zero box-content border-b pb-3">
+              {mileage.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} km
+            </p>
+            <p className="text-white tabular-nums ordinal slashed-zero box-content border-b pb-3">
+              {exteriorColor}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import dynamic from 'next/dynamic'
-import Link from 'next/link'
 import { LocalBusinessJsonLd } from 'next-seo'
 
 import WEB from '../web.config'
@@ -7,34 +6,65 @@ import { getAllPosts, getAllTagsFromPosts } from '../lib/notion'
 import YoutubeList from '../components/youtube-homepage'
 
 export async function getStaticProps () {
+  function arraySort (f) {
+    for (let i = this.length; i;) {
+      const o = this[--i]
+      this[i] = [].concat(f.call(o, o, i), o)
+    }
+    this.sort(function (a, b) {
+      for (let i = 0, len = a.length; i < len; ++i) {
+        if (a[i] !== b[i]) return a[i] < b[i] ? -1 : 1
+      }
+      return 0
+    })
+    for (let i = this.length; i;) {
+      this[--i] = this[i][this[i].length - 1]
+    }
+    return this
+  }
+
+  if (typeof Object.defineProperty === 'function') {
+    try {
+      // eslint-disable-next-line no-extend-native
+      Object.defineProperty(Array.prototype, 'sortBy', { value: arraySort })
+    } catch (e) {
+
+    }
+  }
+  if (!Array.prototype.sortBy) {
+    // eslint-disable-next-line no-extend-native
+    Array.prototype.sortBy = arraySort
+  }
+
   const posts = await getAllPosts({ includePages: false })
   const tags = getAllTagsFromPosts(posts)
+  const post = posts.sortBy((o) => -new Date(o.createdTime))[0]
   return {
     props: {
-      tags
+      tags,
+      post
     },
     revalidate: 1
   }
 }
 
-export default function Home ({ tags }) {
+export default function Home ({ tags, post }) {
   const Carousel = dynamic(
     () => import('../components/carousel.homepage'),
     { ssr: false }
   )
 
   const BrandsCarousel = dynamic(
-    () => import('../components/brands.carousel'),
+    () => import('../components/brands.homepage'),
     { ssr: false }
   )
   const WelcomeText = dynamic(
     () => import('../components/welcomeText'),
     { ssr: false }
   )
-  const slice1 = Object.keys(tags).slice(0, WEB.postsPerPage)
-  const slice2 = Object.keys(tags).slice(
-    WEB.postsPerPage * (2 - 1),
-    WEB.postsPerPage * 2
+  const LatestPublish = dynamic(
+    () => import('../components/latest-publish.homepage'),
+    { ssr: false }
   )
 
   return (
@@ -42,28 +72,12 @@ export default function Home ({ tags }) {
     <Carousel />
     <WelcomeText />
     <YoutubeList />
-    <div className="max-w-7xl lg:max-w-3xl mx-auto w-screen px-3">
-      <div className="border border border-gray-600 rounded-xl bg-gray-900 overflow-hidden pb-16">
-        <p className="text-sm uppercase text-gray-500 mx-6 mt-10">for enthusiast</p>
-        <h4 className="text-3xl text-gray-300 mx-6 font-semibold mb-8 tracking-wide">The brands you love. From a place you can trust.</h4>
-        <BrandsCarousel tags={slice1} />
-        <div className="h-16 bg-transparent" />
-        <BrandsCarousel tags={slice2} dur={50} />
-        <Link href="/search">
-          <a
-            className="uppercase text-xs mt-10 mx-6 block py-3 px-5 bg-white border border-transparent rounded-md text-red-700 hover:bg-gray-50 font-semibold ring ring-gray-200 ring-opacity-50 ring-offset-4 ring-offset-gray-700"
-          >
-            Explore &rarr;
-          </a>
-        </Link>
+    <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-24 lg:items-start">
+      <LatestPublish post={post} />
+      <div className="px-3 mt-3 md:px-0 md:mt-0">
+        <BrandsCarousel tags={tags} />
       </div>
     </div>
-    <div className="max-w-7xl mx-auto bg-white w-28 h-1 rounded-xl my-6" />
-    {/* <h3
-      className="uppercase text-xl text-gray-400 px-3 font-semibold py-6 flex items-center justify-center text-center before:border-4 before:w-[10%] before:border-red-600 before:flex-grow before:transform before:-skew-x-12 before:mr-10 after:border-4 after:w-[10%] after:border-red-600 after:flex-grow after:transform after:skew-x-12 after:ml-10"
-    >
-      Features
-    </h3> */}
     <div className="max-w-7xl text-white mx-auto w-full px-3 py-6">
       <h1 className="text-3xl mb-3">Why Us?</h1>
       <p className="text-xs">Luxoticars, we pride ourselves through the industry’s recognition as one of the pioneer retailers of classic, rare, collectible & exotic cars. With an extensive international network & experience in sourcing, purchasing and selling vehicles to customers worldwide, our team is well prepared to provide fully tailored services for buyers on their next acquisition.</p>
