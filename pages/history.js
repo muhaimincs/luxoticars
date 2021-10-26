ee480869f20f293d2e3d11de7f91bb897b4a3051
Muhaimin CS <muhaimincs@gmail.com>
2021-10-26 17:15:07 +0800

    improve SEO

--- pages/history.js
+++ pages/history.js
@@ -1,18 +1,14 @@
-/* eslint-disable @next/next/no-img-element */
 import dynamic from 'next/dynamic'
-import Head from 'next/head'
-// import Image from 'next/image'
+import { NextSeo } from 'next-seo'
+
 import Layout from '../components/layout.homepage'
+import WEB from '../web.config'
 export default function HistoryPage () {
   const History = dynamic(
     () => import('../components/history'),
     { ssr: false }
   )
-  return (
-    <>
-    <History />
-    </>
-  )
+  return <History />
 }
 
 HistoryPage.getLayout = function getLayout (page) {
@@ -23,26 +19,16 @@ HistoryPage.getLayout = function getLayout (page) {
 
   return (
     <>
-      <Head>
-        <title>History • Luxoticars</title>
-        <meta name="description" content="A TIMELESS INTERPRETATION OF THE AUTOMOTIVE SALES ART" />
-        <meta property="og:locale" content="en-GB" />
-        <meta property="og:title" content="Luxoticars History" />
-        <meta property="og:description" content="A TIMELESS INTERPRETATION OF THE AUTOMOTIVE SALES ART" />
-        <meta property="og:url" content="https://luxoticars.my/history" />
-        <meta
-          property="og:image"
-          content="https://images.ctfassets.net/ijuxqf6x1pz2/4Ybp8mYNmbsNWy2JdtpPrV/896c5652069d5ba2d7bece9e194e84be/photo_2021-09-09_22.07.56.jpeg"
-        />
-        <meta property="og:type" content="profile" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:description" content="A TIMELESS INTERPRETATION OF THE AUTOMOTIVE SALES ART" />
-        <meta name="twitter:title" content="Luxoticars History" />
-        <meta
-          name="twitter:image"
-          content="https://images.ctfassets.net/ijuxqf6x1pz2/4Ybp8mYNmbsNWy2JdtpPrV/896c5652069d5ba2d7bece9e194e84be/photo_2021-09-09_22.07.56.jpeg"
-        />
-      </Head>
+      <NextSeo
+        title={`History • ${WEB.name}`}
+        description="History of LUXOTICARS"
+        canonical={`${WEB.link}/gallery`}
+        openGraph={{
+          url: `${WEB.link}/gallery`,
+          title: `History • ${WEB.name}`,
+          description: 'History of LUXOTICARS'
+        }}
+      />
       <Layout>
         <Header />
         {page}
