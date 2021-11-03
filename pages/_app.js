5fc741cb5a28ab6be9289c8f7e8b88bc31c5c723
Muhaimin CS <muhaimincs@gmail.com>
2021-11-03 22:13:01 +0800

    add Google Tag Manager

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,27 @@ import '../styles/notion.css'
 import 'swiper/css/bundle'
 import 'swiper/css'
 
+import { useEffect } from 'react'
+import Script from 'next/script'
 import dynamic from 'next/dynamic'
 import { DefaultSeo } from 'next-seo'
+import { useRouter } from 'next/router'
+
 import WEB from '../web.config'
+import { GTM_ID, pageview } from '../lib/gtm'
 
 const Gtag = dynamic(() => import('../components/Gtag'), { ssr: false })
 
 function MyApp ({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
+  const router = useRouter()
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', pageview)
+    return () => {
+      router.events.off('routeChangeComplete', pageview)
+    }
+  }, [router.events])
 
   return getLayout(
     <>
@@ -67,6 +80,18 @@ function MyApp ({ Component, pageProps }) {
         ]}
       />
       <Gtag />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   )
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GTM_ID } from '../lib/gtm'
 
 class MyDocument extends Document {
   render () {
@@ -6,6 +7,14 @@ class MyDocument extends Document {
       <Html>
         <Head />
         <body className="bg-black">
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
           <Main />
           <NextScript />
         </body>
