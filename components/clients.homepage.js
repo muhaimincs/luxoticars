9ee6f7dc7babbf7760c686e75a6940c77866a931
Muhaimin CS <muhaimincs@gmail.com>
2022-03-18 20:38:16 +0800

    add more

--- components/clients.homepage.js
+++ components/clients.homepage.js
@@ -43,15 +43,13 @@ const slide1 = [
   'https://images.ctfassets.net/ijuxqf6x1pz2/4qoKLhWYD3IXfrIHTUWi4o/1d81807324a2ea309266cd5d54473d99/photo_2022-02-22_19.40.36.jpeg',
   'https://images.ctfassets.net/ijuxqf6x1pz2/5dT5DNrQCDRmnVl7VP5g5W/d91e9fa0de3269849a278f71c34b26f2/photo_2022-02-22_19.40.27.jpeg',
   'https://images.ctfassets.net/ijuxqf6x1pz2/4yd8Pn0MfojN0W5CrRW2ZQ/1034664a2f5f4d315691144936cdcced/photo_2022-02-22_19.40.17.jpeg',
-  // 'https://images.ctfassets.net/ijuxqf6x1pz2/1iPVcgMw4uL6AtwKNdoVOB/c144ebf5cfceaedbb8d5fa8528defb2f/photo_2021-10-20_10.34.05.jpeg',
-  // 'https://images.ctfassets.net/ijuxqf6x1pz2/7GFJfe8VdZygBgh0tMMCcG/3d4c18643a5207df0fabb62ff68462ca/photo_2021-10-20_10.29.07.jpeg',
-  // 'https://images.ctfassets.net/ijuxqf6x1pz2/6JvKtacC0JYN9oirQvWFfe/97606945e422c04bd9d802bff3c82767/photo_2021-10-20_10.34.09.jpeg',
-  // 'https://images.ctfassets.net/ijuxqf6x1pz2/bhOxdKOVomWgbZcKrKq5N/8e38c71b26b3913d0346ce32b561cf5c/photo_2021-10-20_10.29.02.jpeg'
+  'https://images.ctfassets.net/ijuxqf6x1pz2/2TSVQ6Bd3NstS6tqjgWfDW/d7e568d29e67480acfd2382c7c039ecf/photo_2022-03-18_20.30.14.jpeg',
 ]
 const slide2 = [
   'https://images.ctfassets.net/ijuxqf6x1pz2/5vSho2RLl6tqARVD4FUrhp/1e683c26eeb6e5facd2137e39a032eda/photo_2021-10-22_21.31.32.jpeg',
   'https://images.ctfassets.net/ijuxqf6x1pz2/5BB4e4cU3uj6KR9Cy5cfcd/977a0b1125b1a614538b5af6f7c83269/photo_2021-10-22_21.31.22.jpeg',
-  'https://images.ctfassets.net/ijuxqf6x1pz2/fMhPBcOXwDTJFfvFtOglD/d232c251b52ab707f9f702347b1deb60/photo_2021-10-22_21.31.26.jpeg'
+  'https://images.ctfassets.net/ijuxqf6x1pz2/fMhPBcOXwDTJFfvFtOglD/d232c251b52ab707f9f702347b1deb60/photo_2021-10-22_21.31.26.jpeg',
+  'https://images.ctfassets.net/ijuxqf6x1pz2/4yK2Yx4G8HZymh3YLf8cw4/c60f2cb1c283069d38fe1ad4d1cf1cb6/photo_2022-03-18_20.30.03.jpeg'
 ]
 const slide3 = [
   'https://images.ctfassets.net/ijuxqf6x1pz2/5Oe4BfjB43Mo9Rh34aYyex/7e8b2fb377f886ceac72d8639ab2ee96/photo_2021-10-20_10.21.59.jpeg',
