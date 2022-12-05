b06477ccefe6e5601b597da3252682c260d6f01b
Muhaimin CS <muhaimincs@gmail.com>
2022-12-05 14:11:15 +0800

    fix car

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -32,7 +32,7 @@ export function Car({ car }) {
       <div className="sticky top-0 left-0 right-0 w-full h-screen max-w-7xl max-h-screen min-h-screen mx-auto">
         <div className="relative">
           <div
-            className='absolute top-[40rem] md:top-[60rem] right-0 left-0'
+            className='absolute top-[26rem] md:top-[38rem] right-0 left-0'
             // initial={{ opacity: 0, translateY: 100 }}
             // whileInView={{ opacity: 1, translateY: 10 }}
             // transition={{
@@ -41,8 +41,8 @@ export function Car({ car }) {
             //   damping: 40
             // }}
           >
-            <div className='mx-auto max-w-7xl'>
-              <h2 className="text-2xl md:text-3xl lg:text-5xl lg:text-center font-medium text-white uppercase px-4 tracking-tight">
+            <div className='mx-auto max-w-sm md:max-w-7xl'>
+              <h2 className="text-2xl md:text-3xl lg:text-5xl font-medium text-white uppercase px-4 tracking-tight">
                 <span
                   className={`bg-clip-text text-transparent bg-gradient-to-b backdrop-opacity-10 from-white via-zinc-200 to-transparent`}
                 >
@@ -50,30 +50,31 @@ export function Car({ car }) {
                 </span>
               </h2>
             </div>
-            <div className="relative mx-auto max-w-7xl px-4 py-0 overflow-hidden">
-              <div className="flex justify-between items-center before:block before:absolute before:inset-0 before:bg-[length:15px_15px] before:backdrop-brightness-90 before:backdrop-blur-[20px] footer--homepage-item">
-                <div className="flex-1 overflow-hidden h-[65px]">
-                  <div className="animate-text-slidedown">
-                    {car.highlights.map((highlight) => (
-                      <div key={highlight} className="relative h-[45px] text-ellipsis overflow-hidden uppercase mt-3">
-                        <div className="text-white inline-block leading-10 font-mono line-clamp-1">{highlight}</div>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-                <div className="relative">
-                  <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75 md:hidden"></span>
-                  <Link
-                    href={`/m/${car.slug}#display`}
-                    className="shrink-0 z-10 p-6 md:px-3 md:py-2 flex items-center justify-center rounded-full bg-white shadow-md shadow-red-800/5 ring-1 ring-red-600 transition dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
-                  >
-                    <div className="text-xs font-medium text-zinc-800 dark:text-zinc-100 hidden sm:block">Further Information</div>
-                    <div className="font-medium text-zinc-800 dark:text-zinc-100 block sm:hidden">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
-                      </svg>
+            <div className="relative mx-auto max-w-7xl px-4 py-0 h-10 flex items-center">
+              <div className="w-full before:block before:absolute before:inset-0 before:bg-[length:15px_15px] before:backdrop-brightness-90 before:backdrop-blur-[20px] footer--homepage-item">
+                <div className="flex justify-between items-center">
+                  <div className="grow overflow-hidden h-[65px]">
+                    <div className="animate-text-slidedown">
+                      {car.highlights.map((highlight) => (
+                        <div key={highlight} className="relative h-[45px] text-ellipsis overflow-hidden uppercase mt-3">
+                          <div className="text-white inline-block leading-10 font-mono line-clamp-1">{highlight}</div>
+                        </div>
+                      ))}
                     </div>
-                  </Link>
+                  </div>
+                  <div className="flex-none z-10 relative">
+                    <Link
+                      href={`/m/${car.slug}#display`}
+                      className="p-6 md:px-3 md:py-2 flex items-center justify-center rounded-full shadow-md shadow-red-800/5 ring-slate-300 transition border border-zinc-700/50 bg-zinc-800 ring-0 ring-white/10 hover:border-zinc-700 hover:ring-white/20"
+                    >
+                      <div className="text-xs font-medium text-zinc-800 dark:text-zinc-100 hidden sm:block">Further Information</div>
+                      <div className="font-medium text-zinc-500 dark:text-zinc-100 block sm:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
+                        </svg>
+                      </div>
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
@@ -89,7 +90,7 @@ export function Car({ car }) {
         />
       </div>
       <div
-        className="h-[50vh]"
+        className="h-[70vh]"
         id="Car Model"
         aria-labelledby="car-model"
       />
