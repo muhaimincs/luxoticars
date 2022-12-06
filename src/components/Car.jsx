bbe2955b1deeac64b77a120074d81a1eb449302c
Muhaimin CS <muhaimincs@gmail.com>
2022-12-06 22:00:04 +0800

    fix spacing

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -70,7 +70,7 @@ export function Car({ car }) {
                   </div>
                 </div>
               </div>
-              <div class="flex justify-center mt-10">
+              <div class="flex justify-center mt-4 md:mt-10">
                 <div class="animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center">
                   <svg class="w-6 h-6 text-zinc-500" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                     <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
