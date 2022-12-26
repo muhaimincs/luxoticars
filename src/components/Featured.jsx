58aa8ba2aabc16d949a7350fb47689e0272f53fa
Muhaimin CS <muhaimincs@gmail.com>
2022-12-26 13:49:41 +0800

    card design

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,10 +1,26 @@
 
 export function Featured({ src, title = "", features = [] }) {
   return (
-    <div>
-      <figure className="relative animate-fade-in">
-        <img src={src} alt="Featured car"  />
+    <div className="relative pb-10 md:pb-0">
+      <figure className="animate-fade-in">
+        <img src={src} alt="Featured car" className="rounded-xl"  />
       </figure>
+      <div className="absolute top-2 left-3 md:top-36 z-20">
+        <h2 className="text-zinc-300 text-2xl md:text-7xl">
+          <span className="bg-clip-text text-transparent bg-gradient-to-b backdrop-opacity-10 from-white via-zinc-200 to-transparent">{title}</span>
+        </h2>
+        <div className="flex flex-wrap md:gap-3">
+          {features.map((feature, index) => (
+            <div key={index} className="flex md:items-center gap-2">
+              <span className="text-zinc-300 text-2xl">◦</span>
+              <span className="text-zinc-300 text-base">{feature}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="absolute -inset-px bg-black/50" />
+      <div className="absolute -inset-px bg-gradient-to-b from-transparent via-transparent to-black" />
+      <div className="absolute -inset-px hidden bg-gradient-to-l from-transparent via-transparent to-black lg:block" />
     </div>
   )
 }
