f3509eb0c47220c448b0393999d6f54417cbeaff
Muhaimin CS <muhaimincs@gmail.com>
2023-03-07 20:38:03 +0800

    no photo

--- src/app/client/page.jsx
+++ src/app/client/page.jsx
@@ -62,22 +62,42 @@ export default function ClientPage() {
     <>
       <HeroPattern />
       <Container className="mt-16 sm:mt-32">
-        <h1 className="hidden xl:block text-xl font-bold tracking-tight text-zinc-200 sm:text-3xl">
+        <h1 className="text-xl font-bold tracking-tight text-zinc-200 sm:text-3xl">
           Clients
         </h1>
-        <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {data.map((item, index) => (
-            <article key={index} className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
-              <Image src={item.images} alt={item.name} className="absolute inset-0 -z-10 h-full w-full object-cover" />
-              <div className="absolute inset-0 -z-10 bg-gradient-to-t from-zinc-900 via-zinc-900/40" />
-              <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-zinc-900/10" />
-              <h3 className='mt-3 text-lg font-semibold leading-6 text-white'>
-                <Link href={`/client/${item.url}`}>
-                  {item.name}
+        <div className="mx-auto grid max-w-7xl grid-cols-1 gap-x-8 gap-y-12 sm:gap-y-16 lg:grid-cols-2">
+          <article className='mx-auto w-full max-w-2xl lg:mx-0 lg:max-w-lg'>
+            <h2 id="featured-post" className='mt-4 text-3xl font-bold tracking-tight text-gray-300 sm:text-4xl'>
+              {data[0].name}
+            </h2>
+            <p className="mt-4 text-lg leading-8 text-gray-200">{data[0].description}</p>
+            <div className="mt-4 flex flex-col justify-between gap-6 sm:mt-8 sm:flex-row-reverse sm:gap-8 lg:mt-4 lg:flex-col">
+              <div className="flex">
+                <Link href={`/client/${data[0].url}`} className="text-sm font-semibold leading-6 text-red-600">
+                  Baca Selengkapnya
                 </Link>
-              </h3>
-            </article>
-          ))}
+              </div>
+            </div>
+          </article>
+          <div className="mx-auto w-full max-w-2xl border-t border-gray-900/10 pt-12 sm:pt-16 lg:mx-0 lg:max-w-none lg:border-t-0 lg:pt-0">
+            <div className="-my-12 divide-y divide-gray-900/10">
+              {data.slice(1).map((item) => (
+                <article className="py-12">
+                  <div className="group relative max-w-xl">
+                    <h2 className="mt-2 text-lg font-semibold text-gray-300 group-hover:text-red-600">
+                      <Link href={`/client/${item.url}`}>
+                        <span className='absolute inset-0' />
+                        {item.name}
+                      </Link>
+                    </h2>
+                    <p className="mt-4 text-sm leading-6 text-gray-400">
+                      {item.description}
+                    </p>
+                  </div>
+                </article>
+              ))}
+            </div>
+          </div>
         </div>
       </Container>
       <div className="py-10">
