b6f3a7c72a3fbf118a393ffbc82effb8e90ea875
Muhaimin CS <muhaimincs@gmail.com>
2023-05-10 14:31:30 +0800

    use OG

---
+++ src/app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.luxoticars.cc',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.luxoticars.cc/about',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.luxoticars.cc/client',
+      lastModified: new Date(),
+    },
+  ];
+}
